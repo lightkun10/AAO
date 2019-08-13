75a870390ecaffc6809ee336f77435f6d1314f9c
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-08-13 09:16:45 -0700

    Complete hotel part 1

--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,36 @@
 class Room
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
 
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length < @capacity
+      false
+    else
+      true
+    end
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    puts @occupants
+    if !full?
+      @occupants << name
+      true
+    else
+      false
+    end
+  end
 end
