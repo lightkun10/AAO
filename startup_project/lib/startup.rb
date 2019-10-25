d0a508ea90cd38e6fe69aa9e39cc94a6aac60660
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-25 14:29:26 -0700

    Finish partway

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,20 @@
 require "employee"
 
 class Startup
+  attr_reader :name, :funding, :salaries, :employees
 
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end
+
+  def valid_title?(title)
+    @salaries.has_key?(title)
+  end
+
+  def >(other)
+    self.funding > other.funding
+  end
 end
