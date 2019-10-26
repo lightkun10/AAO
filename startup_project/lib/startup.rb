4930a0d548dbf01f228bd770fce1fbbc4bafbf35
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-25 21:10:25 -0700

    Commit phase 2 of startup exercise.

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -17,4 +17,32 @@ class Startup
   def >(other)
     self.funding > other.funding
   end
+
+  def hire(employee_name, title)
+    if valid_title?(title)
+      @employees << Employee.new(employee_name, title)
+    else
+      raise "Title invalid"
+    end
+  end
+
+  def size
+    @employees.length
+  end
+
+  def pay_employee(employee)
+    owed = @salaries[employee.title]
+    if @funding >= owed
+      employee.pay(owed)
+      @funding -= owed
+    else
+      raise "Insufficient funds"
+    end
+  end
+
+  def payday
+    @employees.each do |employee|
+      self.pay_employee(employee)
+    end
+  end
 end
