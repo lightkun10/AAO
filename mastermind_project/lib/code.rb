6926b1210c5318f99f9a4310f7373368d70b8741
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-28 13:40:55 -0700

    Complete most of part 1

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,22 +1,32 @@
 class Code
+  POSSIBLE_PEGS = {
+  "R" => :red,
+  "G" => :green,
+  "B" => :blue,
+  "Y" => :yellow,
+}
+  
   attr_reader :pegs
 
-  POSSIBLE_PEGS = {
-    "R" => :red,
-    "G" => :green,
-    "B" => :blue,
-    "Y" => :yellow
-  }
+  def initialize(pegs)
+    @pegs = pegs.map(&:upcase)
+    if !Code.valid_pegs?(pegs)
+      raise 'invalid pegs'
+    end
+  end
 
   def self.valid_pegs?(characters)
     characters.each do |character|
-        if !POSSIBLE_PEGS.include?(character.upcase)
-          
-          return false
-        end
-      # { |char| Code::POSSIBLE_PEGS.include?(char.upcase) }
+      if !POSSIBLE_PEGS.include?  (character.upcase)
+        return false
+      end
     end
     true
   end
 
+  def self.random(number)
+    pegs = Array.new(number) {
+      POSSIBLE_PEGS.keys.sample}
+    Code.new(pegs)
+  end
 end
