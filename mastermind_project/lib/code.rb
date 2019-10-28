7f1bc8019e82970ab440843f5ddf9a6f8fa47fcf
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-28 11:25:13 -0700

    complete first spec for mastermind

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,9 +1,22 @@
 class Code
+  attr_reader :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
     "B" => :blue,
     "Y" => :yellow
   }
+
+  def self.valid_pegs?(characters)
+    characters.each do |character|
+        if !POSSIBLE_PEGS.include?(character.upcase)
+          
+          return false
+        end
+      # { |char| Code::POSSIBLE_PEGS.include?(char.upcase) }
+    end
+    true
+  end
 
 end
