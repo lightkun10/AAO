973975a91913bc1bfa98efe7de9719cb5fa4bc35
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-10-29 13:48:29 -0700

    Start part 2

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,23 +1,23 @@
 class Code
   POSSIBLE_PEGS = {
-  "R" => :red,
-  "G" => :green,
-  "B" => :blue,
-  "Y" => :yellow,
-}
-  
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow,
+  }
+
   attr_reader :pegs
 
   def initialize(pegs)
     @pegs = pegs.map(&:upcase)
     if !Code.valid_pegs?(pegs)
-      raise 'invalid pegs'
+      raise "invalid pegs"
     end
   end
 
   def self.valid_pegs?(characters)
     characters.each do |character|
-      if !POSSIBLE_PEGS.include?  (character.upcase)
+      if !POSSIBLE_PEGS.include? (character.upcase)
         return false
       end
     end
@@ -26,12 +26,13 @@ class Code
 
   def self.random(number)
     pegs = Array.new(number) {
-      POSSIBLE_PEGS.keys.sample}
+      POSSIBLE_PEGS.keys.sample
+    }
     Code.new(pegs)
   end
 
   def self.from_string(string)
-    Code.new(string.split(''))
+    Code.new(string.split(""))
   end
 
   def [](idx)
@@ -42,4 +43,13 @@ class Code
     @pegs.length
   end
 
+  def num_exact_matches(guess)
+    count = 0
+    i = 0
+    while i < guess.length
+      count += 1 if guess[i] == @pegs[i]
+      i += 1
+    end
+    count
+  end
 end
